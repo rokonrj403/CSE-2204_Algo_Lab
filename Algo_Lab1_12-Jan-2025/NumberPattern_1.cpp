01deda80454ce5ab8ea590698dcaf80f440b0981
rokonrj403 <rokonuzzamannur403@gmail.com>
2025-01-13 02:02:33 +0600

    2nd Problem Solved

---
+++ Algo_Lab1_12-Jan-2025/NumberPattern_1.cpp
@@ -0,0 +1,29 @@
+// To Print the following pattern till N number of user input
+
+// 1
+// 22
+// 333
+// 4444
+// NNNNN
+
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int numberN;
+
+
+    cout << "Enter the N: ";
+    cin >> numberN;
+
+    for (int i = 1; i <= numberN; i++){
+
+        for (int j = 1; j <= i; j++){
+            cout << i;
+        }
+        cout << endl;
+    }
+}
