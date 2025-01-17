ee1fee7a717dd5bf7d7fbaa639d470f84e842692
rokonrj403 <rokonuzzamannur403@gmail.com>
2025-01-17 19:06:53 +0600

    2D Matrix Addition Solved

---
+++ Algo_Lab1_12-Jan-2025/Matrices_Addition.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int row1, row2, col1, col2;
+
+
+
+    cout << "Number of Row for Matrix A: ";
+    cin >> row1;
+    cout << "Number of Column for Matrix A: ";
+    cin >> col1;
+
+    int matrixA [row1][col1];
+    // int matrixB [row2][col2];
+
+    cout << "Enter the Element of Matrix A(" << row1 << "x" << col1 <<")" << ":" << endl;
+
+
+    for (int i = 0; i < row1; i++){
+
+        for (int j = 0; j < col1; j++){
+            cin>> matrixA[i][j];
+        }
+    }
+
+    cout << "Number of Row for Matrix A: ";
+    cin >> row2;
+    cout << "Number of Column for Matrix A: ";
+    cin >> col2;
+
+    int matrixB [row2][col2];
+
+    cout << "Enter the Element of Matrix A(" << row2 << "x" << col2 <<")" << ":" << endl;
+
+    for (int i = 0; i < row2; i++)
+    {
+
+        for (int j = 0; j < col2; j++){
+            cin>> matrixB[i][j];
+        }
+    }
+
+    int matrixC[row1][col1];
+
+    if (row1 == row2 && col1 == col2)
+    {
+
+        for (int i = 0; i < row1; i++){
+            for (int j = 0; j< col1; j++){
+
+                matrixC[i][j] = matrixA[i][j] + matrixB[i][j];
+
+                }
+        }
+        
+        cout << "MatrixA + MatrixB = " << endl;
+
+        for (int i = 0; i < row1; i++){
+        for(int j = 0; j< col1; j++){
+            cout << matrixC[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+    }
+    else{
+        cout << "ERROR!!! Number of Row or Cloumn for both Matrix is/are Different!" << endl;
+    }
+
+    
+
+
+
+        return 0;
+}
